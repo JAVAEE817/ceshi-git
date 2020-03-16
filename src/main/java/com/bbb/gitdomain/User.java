cdcf2f9d6f4b527f021ea87dbb605edaeea43dd6
bobo <1198410760@qq.com>
2020-03-16 11:55:48 +0800

    111

---
+++ src/main/java/com/bbb/gitdomain/User.java
@@ -0,0 +1,11 @@
+package com.bbb.gitdomain;
+
+/**
+ * @author: create by chenbo
+ * @description: com.bbb.gitdomain
+ * @date:2020/3/16
+ */
+public class User {
+    private String name;
+    private Integer age;
+}
